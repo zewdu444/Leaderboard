0106d2289c4f1dedc3dc1c9d40e47bbf03daf29f
zewdu erkyhun <zewdu444@gmail.com>
2023-01-31 23:23:47 +0300

    Fix refresh and display score

--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
 import './style.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import sendScore from './module/sendScore.js';
+import getScore from './module/getScore.js';
 import displayScore from './module/displayScore.js';
 
 const scoreSubmit = document.getElementById('scoresubmit');
 const scoreUser = document.getElementById('user');
 const score = document.getElementById('score');
 const scoreRefresh = document.getElementById('refresh');
-// initial display score
-displayScore();
-// submit score
-const submitScore = async () => {
-  scoreSubmit.addEventListener('click', async (e) => {
-    if (scoreUser.value.trim() !== '' && +score.value.trim() !== '') {
-      await sendScore({ user: scoreUser.value.trim(), score: +score.value.trim() });
-      scoreUser.value = '';
-      score.value = '';
-      displayScore();
-    } else {
-      e.preventDefault();
-    }
-  });
-};
-// refresh score
-const refreshScore = async () => {
-  scoreRefresh.addEventListener('click', async () => {
-    window.location.reload();
-    displayScore();
-  });
-};
 
-// call submit and refresh score
-submitScore();
-refreshScore();
+// initial display score
+window.addEventListener('DOMContentLoaded', () => {
+  displayScore();
+});
+// submit score
+scoreSubmit.addEventListener('click', async (e) => {
+  if (scoreUser.value.trim() !== '' && +score.value.trim() !== '') {
+    await sendScore({ user: scoreUser.value.trim(), score: +score.value.trim() });
+    scoreUser.value = '';
+    score.value = '';
+  } else {
+    e.preventDefault();
+  }
+});
+
+// refresh score
+scoreRefresh.addEventListener('click', async () => {
+  window.location.reload();
+  await getScore();
+  displayScore();
+});
