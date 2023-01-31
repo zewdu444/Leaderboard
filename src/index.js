5277f33c5496ebbadc4de372cdc54fb4521156e5
zewdu erkyhun <zewdu444@gmail.com>
2023-01-31 22:15:39 +0300

    Add display and index implementation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import './style.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
-import getScore from './module/getScore.js';
 import sendScore from './module/sendScore.js';
+import displayScore from './module/displayScore.js';
 
-const displayScore = async () => {
-  // await sendScore({ user: 'zewdu erkyhun', score: 30 });
-  console.log(await getScore());
-};
+const scoreSubmit = document.getElementById('scoresubmit');
+const scoreUser = document.getElementById('user');
+const score = document.getElementById('score');
+const scoreRefresh = document.getElementById('refresh');
+// initial display score
 displayScore();
+// submit score
+const submitScore = async () => {
+  scoreSubmit.addEventListener('click', async (e) => {
+    if (scoreUser.value.trim() !== '' && +score.value.trim() !== '') {
+      await sendScore({ user: scoreUser.value.trim(), score: +score.value.trim() });
+      scoreUser.value = '';
+      score.value = '';
+      displayScore();
+    } else {
+      e.preventDefault();
+    }
+  });
+};
+// refresh score
+const refreshScore = async () => {
+  scoreRefresh.addEventListener('click', async () => {
+    window.location.reload();
+    displayScore();
+  });
+};
+
+// call submit and refresh score
+submitScore();
+refreshScore();
